4db880fa3a04391396dd4af1857da91ed207d307
ex_ode <56847003+Ex0D@users.noreply.github.com>
2024-04-05 13:36:34 +0200

    Fix next error : TypeError: Cannot read properties of null (reading 'vip')

--- twitch-bot/utils/getRole.js
+++ twitch-bot/utils/getRole.js
@@ -6,7 +6,7 @@ function getRole(user, channel)
     const formattedChannel = channel.replace('#', '');
     if (user.username === formattedChannel) return USER_ROLES.BROADCASTER;
     if (user.mod) return USER_ROLES.MODERATOR;
-    if (user.badges.vip) return USER_ROLES.VIP;
+    if (user.badges && user.badges.vip) return USER_ROLES.VIP;
 
     return USER_ROLES.VIEWER;
 }
